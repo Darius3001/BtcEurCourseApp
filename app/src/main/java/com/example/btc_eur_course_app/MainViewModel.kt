be99b5b69cc6480c01e61e88f45728773be1327a
Darius3001 <darius.mertin@outlook.com>
2024-02-15 21:13:57 +0100

    initial commit

---
+++ app/src/main/java/com/example/btc_eur_course_app/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.btc_eur_course_app
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+}
---
+++ app/src/main/java/com/example/btc_eur_course_app/datasource/MainRepository.kt
@@ -0,0 +1,5 @@
+package com.example.btc_eur_course_app.datasource
+
+class MainRepository {
+
+}
