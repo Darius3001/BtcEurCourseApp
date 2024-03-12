c8390abbd5453bc23b33eea44383fd1c3ef82d83
Darius3001 <darius.mertin@outlook.com>
2024-03-12 16:18:02 +0100

    wip

--- app/src/main/java/com/example/btc_eur_course_app/MainActivity.kt
+++ app/src/main/java/com/example/btc_eur_course_app/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.btc_eur_course_app.datasource.domain.BitcoinData
 import com.example.btc_eur_course_app.ui.theme.BtcEurCourseAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,11 +42,27 @@ class MainActivity : ComponentActivity() {
 
                     Spacer(modifier = Modifier.height(64.dp))
 
-                    Text(
-                        text = viewModel.state.historicalData.value
-                    )
+                    viewModel.state.historicalData.value.forEach {
+                        DataEntry(data = it)
+                    }
                 }
             }
         }
     }
+}
+
+@Composable
+fun ColumnScope.DataEntry(data: BitcoinData) {
+    Row {
+        Text(
+            modifier = Modifier.weight(1f),
+            text = data.timestamp.toString()
+        )
+
+        Text(
+            modifier = Modifier.weight(1f),
+            text = data.value.toString()
+        )
+    }
+    Divider()
 }
--- app/src/main/java/com/example/btc_eur_course_app/MainState.kt
+++ app/src/main/java/com/example/btc_eur_course_app/MainState.kt
@@ -2,8 +2,9 @@ package com.example.btc_eur_course_app
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import com.example.btc_eur_course_app.datasource.domain.BitcoinData
 
 data class MainState(
     val isConnected: MutableState<Boolean> = mutableStateOf(false),
-    val historicalData: MutableState<String> = mutableStateOf("")
+    val historicalData: MutableState<List<BitcoinData>> = mutableStateOf(listOf())
 )
